8261675446f36b857bd2fd5dc083c6fcdb278921
lutfir2019 <lutfirrahman2019@gmail.com>
2025-02-24 12:58:21 +0700

    feat(components): add shadow to Sidebar component for improved visual depth

--- components/sidebar/Sidebar.vue
+++ components/sidebar/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="hidden md:flex flex-col w-60 bg-gray-900 min-h-screen p-4 pt-20">
+  <div class="hidden md:flex flex-col w-60 bg-gray-900 min-h-screen p-4 pt-20 shadow-md">
     <div class="flex-1">
       <SidebarMain />
     </div>
